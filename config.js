2a5c7bfe8214f7e7888b368c3e3746cf5ebf9d48
chizey97 <chisenga.mali@gmail.com>
2026-06-30 14:53:08 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  businessName: "Zee's Sweet Treats",
+
+  whatsappNumber: "260974451250",
+
+  paymentNumbers: {
+    airtel: "0974451250",
+    mtn: "0963539378"
+  },
+
+  googleScriptUrl: "https://script.google.com/macros/s/YOUR_GOOGLE_SCRIPT_ID/exec",
+
+  pickupLocation: "Lusaka, Zambia",
+
+  currency: "K"
+};
